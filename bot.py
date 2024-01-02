327c55702202bd46043dce6380e72368bcc4b5a0
weldoy <132596720+weldoy@users.noreply.github.com>
2024-01-03 01:16:48 +0300

    added the latest bitcoin rate

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler
 from telegram.ext import CallbackContext, Filters
 
 import logging
+import requests
 
 from key import TOKEN
 from fsm import weather_handler
@@ -22,6 +23,7 @@ def main():
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('help', get_help))
     dp.add_handler(weather_handler)
+    dp.add_handler(CommandHandler('btcvalue', get_btc_value))
     dp.add_handler(MessageHandler(Filters.command, unknown))
 
     updater.start_polling()
@@ -52,13 +54,23 @@ def get_help(update: Update, context: CallbackContext):
         '<b>HELP</b>',
         '',
         f"There is i'll tell u about my arsenal :",
-        '/weather - this will help you if you want to know the weather in cities.',
+        '/weather - that will help you if you want to know the weather in cities.',
+        '/btcvalue - it shows the latest BTC price.',
         '',
         'The rest is in development...'
     ]
     text = '\n'.join(text)
     context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode=ParseMode.HTML,
                              reply_markup=ReplyKeyboardRemove())
+
+
+def get_btc_value(update: Update, context: CallbackContext):
+    logger.info(f'{update.effective_user.username} called the function - get_btc_value')
+    url = 'https://blockchain.info/ru/ticker'
+    req = requests.get(url)
+    req_json = req.json()
+    answer = f'The latest price BTC - {float(req_json["USD"]["last"])}$'
+    context.bot.send_message(update.effective_chat.id, answer, reply_markup=ReplyKeyboardRemove())
 
 
 def unknown(update, context):
